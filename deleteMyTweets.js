d81b5de2e98ac05d214b507dcfc9675eb97da50c
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2021-12-01 09:29:19 +0530

    Update deleteMyTweets.js
    use as browser bookmark

--- deleteMyTweets.js
+++ deleteMyTweets.js
@@ -1,3 +1,5 @@
+javascript:
+
 'use strict';
 
 /* MIT License
@@ -22,25 +24,27 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE. */
 
-var delTweets = function () {
-	var tweetsRemaining = document.querySelectorAll('[role="heading"]+div')[1].textContent;
-	console.log('Remaining: ', tweetsRemaining);
-	window.scrollBy(0, 10000);
-	document.querySelectorAll('[aria-label="More"]').forEach(function (v, i, a) {
-		v.click();
-		document.querySelectorAll('span').forEach(function (v2, i2, a2) {
-			if (v2.textContent === 'Delete') {
-				v2.click();
-				document.querySelectorAll('[data-testid="confirmationSheetConfirm"]').forEach(function (v3, i3, a3) {
-					v3.click();
-				});
-			}
-			else {
-				document.body.click();
-			}
-		});
-	});
-	setTimeout(delTweets, 0);
-}
+(function () {
+    var delTweets = function () {
+        var tweetsRemaining = document.querySelectorAll('[role="heading"]+div')[1].textContent;
+        console.log('Remaining: ', tweetsRemaining);
+        window.scrollBy(0, 10000);
+        document.querySelectorAll('[aria-label="More"]').forEach(function (v, i, a) {
+            v.click();
+            document.querySelectorAll('span').forEach(function (v2, i2, a2) {
+                if (v2.textContent === 'Delete') {
+                    v2.click();
+                    document.querySelectorAll('[data-testid="confirmationSheetConfirm"]').forEach(function (v3, i3, a3) {
+                        v3.click();
+                    });
+                }
+                else {
+                    document.body.click();
+                }
+            });
+        });
+        setTimeout(delTweets, 0);
+    };
 
-delTweets();
+    delTweets();
+})();
